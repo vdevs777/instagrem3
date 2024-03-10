3a463774c8612e0aeb6993ca54ff3f2c73d718ed
unknown <jeivi.777@gmail.com>
2024-03-10 15:12:40 -0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { queryClient } from "@/lib/queryClient";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { QueryClientProvider } from "@tanstack/react-query";
-import type { AppProps } from "next/app";
+import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
