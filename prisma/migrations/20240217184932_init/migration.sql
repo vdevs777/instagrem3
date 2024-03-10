3d95a0ba3eaac62aedbef4f94d1edce0fd7c6557
unknown <jeivi.777@gmail.com>
2024-03-10 14:51:18 -0300

    .

--- prisma/migrations/20240217184932_init/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Login" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "user_telephone_or_email" TEXT NOT NULL,
-    "password" TEXT NOT NULL
-);
---
+++ prisma/migrations/20240310175104_lala/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Informations" (
+    "id" TEXT NOT NULL,
+    "login" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "Informations_pkey" PRIMARY KEY ("id")
+);
